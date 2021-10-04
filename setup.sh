92664587ea17a723334237e218f523db172ef55a
Daniel Widrick <daniel@widrick.net>
2021-10-03 23:36:14 -0400

    setup for terminator and i3

--- setup.sh
+++ setup.sh
@@ -5,3 +5,13 @@ BASEDIR="$(cd "$(dirname "$BASH_SOURCE[0]}")" && pwd)"
 #git
 rm -f ~/.gitconfig
 ln -s ${BASEDIR}/.gitconfig ~/.gitconfig
+
+#i3
+mkdir -p ~/.config/i3
+rm -f ~/.config/i3/config
+ln -s ${BASEDIR}/.config/i3/config ~/.config/i3/config
+
+#terminator
+mkdir -p ~/.config/terminator
+rm -f ~/.config/terminator/config
+ln -s ${BASEDIR}/.config/terminator/config ~/.config/terminator/config
