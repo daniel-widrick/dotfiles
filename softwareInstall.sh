102ca240177bf86825485350cb453657d6603336
Daniel Widrick <daniel@widrick.net>
2021-10-07 22:54:10 +0000

    add vnc server install

--- softwareInstall.sh
+++ softwareInstall.sh
@@ -3,10 +3,6 @@
 ##To be executed as root
 
 
-dnf install -y vim i3 terminator net-tools bind-utils nmap nc dnf-plugins-core
+dnf install -y vim net-tools bind-utils nmap nc dnf-plugins-core
 
 
-#Install Brave-Browser
-dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-dnf install brave-browser
---
+++ vncInstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo dnf install -y tigervnc-server
+sudo cp /usr/lib/systemd/system/vncserver@.service /etc/systemd/system/
+sudo /usr/bin/cp .vnc/vncserver.users /etc/tigervnc/vncserver.users
+sudo dnf install -y terminator i3 
+
+#Install Brave-Browser
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser
+
+cp .vnc/xstartup ~/.vnc/
+cp .vnc/config ~/.vnc/
+
+echo "#############"
+echo "run 'vncserver' to finish setting up vnc server"
+echo "#############"
