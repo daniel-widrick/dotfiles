cc56fc03a42aa207d26143f91089ca2032faddcf
Daniel Widrick <daniel@widrick.net>
2021-11-14 14:29:19 -0500

    dotnet 6.0 install

--- dotnetInstall.sh
+++ dotnetInstall.sh
@@ -7,11 +7,12 @@ sudo curl -OL https://download.jetbrains.com/rider/JetBrains.Rider-2021.2.2.tar.
 sudo tar -xzvf JetBrains.Rider-2021.2.2.tar.gz
 sudo mv /opt/src/JetBrains\ Rider-2021.2.2 /opt/work/rider
 
-sudo curl -OL https://download.visualstudio.microsoft.com/download/pr/20283373-1d83-4879-8278-0afb7fd4035e/56f204f174743b29a656499ad0fc93c3/dotnet-sdk-6.0.100-rc.2.21505.57-linux-x64.tar.gz
+
+sudo curl -OL https://download.visualstudio.microsoft.com/download/pr/17b6759f-1af0-41bc-ab12-209ba0377779/e8d02195dbf1434b940e0f05ae086453/dotnet-sdk-6.0.100-linux-x64.tar.gz
 
 
 sudo mkdir /opt/work/dotnet
 cd /opt/work/dotnet
-sudo tar -xzvf /opt/src/dotnet-sdk-6.0.100-rc.2.21505.57-linux-x64.tar.gz
+sudo tar -xzvf /opt/src/dotnet-sdk-6.0.100-linux-x64.tar.gz
 
 dnf install -y mono-core mono-devel
