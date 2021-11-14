86760b5b4fe6026411a926c648e6e2716c0590a4
Daniel Widrick <daniel@widrick.net>
2021-11-14 14:24:27 -0500

    python changes

--- pythonSetup.sh
+++ pythonSetup.sh
@@ -5,6 +5,6 @@ cd /opt/src
 sudo curl -OL https://download-cdn.jetbrains.com/python/pycharm-professional-2021.2.2.tar.gz
 
 sudo tar -xzvf pycharm-professional-2021.2.2.tar.gz
-sudo mv pycharm-2021.2.2 /opt/work/
+sudo mv pycharm-2021.2.2 /opt/work/pycharm
 
 
