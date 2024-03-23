0524d0255f5950f3e04992d43d14d69bcf25f0f0
lvlint67 <daniel@widrick.net>
2024-03-22 20:40:50 -0400

    Add sudo and install golang w/updates

--- install.sh
+++ install.sh
@@ -5,13 +5,30 @@
 
 # Get apt to be quiet about restarting services
 export DEBIAN_FRONTEND=noninteractive
+export SCRIPT_DIR="$(dirname "$0")"
 
-apt update
-apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
 #Install the basic utils
-apt install -y bind9-utils net-tools dnsutils vim git apt-file \
+sudo apt install -y bind9-utils net-tools dnsutils vim git apt-file file cron \
 	sway swaylock wayvnc xwayland kitty openssl\
 	firefox
+#Install Jet Brains Unofficial Jetbrains repo
+curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | gpg --dearmor | sudo tee /usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg] http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com any main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
+sudo apt-get update
+
+sudo apt install goland -y
+#Install Golang
+mkdir ~/bin
+cd ~/bin
+git clone https://github.com/udhos/update-golang
+cd update-golang
+sudo ./update-golang.sh
+export UPDATE_GOLANG=$(pwd)
+echo "0  4  *  *  *	root	${UPDATE_GOLANG}/update-golang.sh" | sudo tee -a /etc/crontab
+echo 'source /etc/profile.d/golang_path.sh' >> ~/.bashrc
+cd $SCRIPT_DIR
 
 #Configure sway
 mkdir -p ~/.config/{kitty,sway,swaylock}/
