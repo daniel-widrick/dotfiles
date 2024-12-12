34bd38023034db7686ac1995e712e15b1299ee72
Daniel Widrick <daniel@widrick.net>
2024-12-11 22:07:21 -0500

    add submodule support

--- run.bat
+++ run.bat
@@ -1,5 +1,9 @@
 if not exist %USERPROFILE%/.gitconfig (
 	type nul > %USERPROFILE%/.gitconfig
 )
+
+git submodule update --init --recursive
+git submodule update --recursive --remote
+
 docker compose up -d apt-cache
 docker compose up -d linux-dev-desktop --build
--- run.sh
+++ run.sh
@@ -7,5 +7,8 @@ fi
 
 export USERPROFILE=~
 
+git submodule update --init --recursive
+git submodule update --recursive --remote
+
 docker compose up -d apt-cache
 docker compose up -d linux-dev-desktop --build
